761944254221eb5d8acfa0e2beb2f347b8a630bf
西林咲音 <s.n.baton319@icloud.com>
2015-10-28 19:36:29 +0900

    Initial Commit

---
+++ Touch_Album_1028/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Touch_Album_1028
+//
+//  Created by 西林咲音 on 2015/10/28.
+//  Copyright (c) 2015年 西林咲音. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ Touch_Album_1028/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  Touch_Album_1028
+//
+//  Created by 西林咲音 on 2015/10/28.
+//  Copyright (c) 2015年 西林咲音. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
